96959d7736d21e259455f6b9e1007b0fac4d97cb
Jonas Van Hoof <36266973+JonasVanHoof@users.noreply.github.com>
2018-11-23 11:48:49 +0100

    initial commit

---
+++ app/src/main/java/vanhoof/jonas/dev/pm_testapp/MainActivity.java
@@ -0,0 +1,13 @@
+package vanhoof.jonas.dev.pm_testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/vanhoof/jonas/dev/pm_testapp/Utils.java
@@ -0,0 +1,8 @@
+package vanhoof.jonas.dev.pm_testapp;
+
+public class Utils {
+    public static int addNumbers(int first, int second) {
+
+        return first + second;
+    }
+}
